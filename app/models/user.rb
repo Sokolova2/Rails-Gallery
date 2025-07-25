2057f95b46890de1e07bee97fe03e99d49087708
Karina <karinasokolova252@gmail.com>
2025-07-25 13:27:13 +0300

    Made sending a message to the mail

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,25 @@
 class User < ApplicationRecord
+  include MailForm::Delivery
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
+
+  def headers
+    {
+      :subject => 'Welcome to Gallery',
+      :from => 'apponrails2025@gmail.com',
+      :to => email
+    }
+  end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.first_name = auth.info.first_name
+      user.last_name = auth.info.last_name
+    end
+  end
 end
