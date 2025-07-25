90b278bc3c76e933bd1521bb2743bb62a8c3fa94
Karina <karinasokolova252@gmail.com>
2025-07-26 00:58:01 +0300

    Added avatar for users and connected bootstrap

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def user_avatar(user)
+    if user.avatar?
+      user.avatar.url
+    else
+      asset_path('user.png')
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,19 @@
 class User < ApplicationRecord
   include MailForm::Delivery
+
+  mount_uploader :avatar, AvatarUploader
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :omniauthable, :omniauth_providers => [:google_oauth2]
 
+  validates_integrity_of :avatar
+  validates_processing_of :avatar
+
   def headers
     {
-      :subject => 'Welcome to Gallery',
+      :subject => 'Welcome to Rails Gallery. We nice to see you! You can log in follow the link: http://localhost:3000/users/sign_in',
       :from => 'apponrails2025@gmail.com',
       :to => email
     }
@@ -20,6 +25,7 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.first_name = auth.info.first_name
       user.last_name = auth.info.last_name
+      user.avatar = auth.info.image
     end
   end
 end
