fd7a7e4a5cce3ea66eea1a9ce9968b58cbcebae4
Karina <karinasokolova252@gmail.com>
2025-07-31 01:38:50 +0300

    Add event listener for automatic upload photo without click on button

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -6,3 +6,16 @@ import "@popperjs/core";
 
 console.log(bootstrap.Modal)
 Popper.createPopper()
+
+document.addEventListener("DOMContentLoaded", function(){
+    const fileInput = document.getElementById("image_upload");
+    const form = document.getElementById("image-upload-form");
+
+    if(fileInput && form){
+        fileInput.addEventListener("change", function (){
+            if (fileInput.files.length > 0){
+                form.submit();
+            }
+        });
+    }
+});
