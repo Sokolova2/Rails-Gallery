1a1effc2f0462b8f471e04d1253169d6c7ce9494
Karina <karinasokolova252@gmail.com>
2025-07-31 23:46:58 +0300

    Add helpers for preview images

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,10 @@
 module CategoriesHelper
+  def preview_category(category, options = { size: 200 })
+    size = options[:size]
+    if category.images.any?
+      image_tag category.images.first.images.url, size: size, class: "rounded-5"
+    else
+      image_tag "preview.png", size: size, class: "rounded-5"
+    end
+  end
 end
---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,10 @@
+module UserHelper
+  def avatar_for(user, options = { size: 50 })
+    size = options[:size]
+    if user.avatar?
+      image_tag user.avatar.url, size: size, class: "rounded-circle"
+    else
+      image_tag "avatar.png", size: size, class: "rounded-circle"
+    end
+  end
+end
