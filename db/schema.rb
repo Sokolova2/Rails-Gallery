a3615d0efd0fe4ded50164cb98441ccdb429c477
Karina <karinasokolova252@gmail.com>
2025-08-19 20:38:25 +0300

    Add a new model for using friendly id

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_19_110711) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_19_170945) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_110711) do
     t.string "category_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_categories_on_slug", unique: true
     t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
@@ -32,6 +34,17 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_19_110711) do
     t.index ["category_id"], name: "index_comments_on_category_id"
     t.index ["image_id"], name: "index_comments_on_image_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string "slug", null: false
+    t.integer "sluggable_id", null: false
+    t.string "sluggable_type", limit: 50
+    t.string "scope"
+    t.datetime "created_at"
+    t.index ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true
+    t.index ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type"
+    t.index ["sluggable_type", "sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_type_and_sluggable_id"
   end
 
   create_table "images", force: :cascade do |t|
