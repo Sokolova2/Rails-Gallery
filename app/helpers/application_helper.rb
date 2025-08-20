7e80fa0920384df3b06abb30647d5d6bc8e8a569
Karina <karinasokolova252@gmail.com>
2025-08-20 20:18:17 +0300

    Remove unnecessary method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,4 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def user_avatar(user)
-    if user.avatar?
-      user.avatar.url
-    else
-      asset_path('user.png')
-    end
-  end
 end
