a1c15f829af02d1af3758e86237bbb4af4912330
Karina <karinasokolova252@gmail.com>
2025-08-25 20:12:05 +0000

    Update size for images

--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
 module CategoriesHelper
-  def preview_category(category, options = { size: 200 })
+  def preview_category(category, options = { size: 300 })
     size = options[:size]
     if category.images.any?
-      image_tag category.images.first.image.url, size: size, class: "rounded-4"
+      image_tag category.images.first.image.url, size: size, class: "image-preview rounded-4"
     else
-      image_tag "preview.png", size: size, class: "rounded-4"
+      image_tag "preview.png", size: size, class: "image-preview rounded-4"
     end
   end
 end
