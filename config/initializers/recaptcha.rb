726e3a9a7059d19588add713ef44f2c5199ffbe1
Karina <karinasokolova252@gmail.com>
2025-08-26 19:45:38 +0300

    Add configure for recaptcha

---
+++ config/initializers/recaptcha.rb
@@ -0,0 +1,7 @@
+# config/initializers/recaptcha.rb
+# frozen_string_literal: true
+
+Recaptcha.configure do |config|
+  config.site_key = ENV.fetch('RECAPTCHA_SITE_KEY', nil)
+  config.secret_key = ENV.fetch('RECAPTCHA_SECRET_KEY', nil)
+end
