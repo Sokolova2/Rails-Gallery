b7793d0cf7d0881a77836af20e620acda1060d37
Karina <karinasokolova252@gmail.com>
2025-08-26 16:18:33 +0300

    Add seed for add likes and comments for images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,17 @@
 # frozen_string_literal: true
+
+user = User.find_by(first_name: 'default')
+cat_category = Category.find_by(slug: 'cats')
+
+images = Image.where(category: cat_category)
+
+content_comments = ["it's perfect", "so pretty", "so beautiful!!!", "cuteeee!!!", "cool", "like this"]
+
+images.each do |image|
+  image.likes.create(user: user, category: cat_category)
+  content_comments.each do |comment|
+    image.comments.create(user: user, category: cat_category, comment: comment)
+  end
+end
+
+
