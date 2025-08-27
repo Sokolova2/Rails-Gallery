77aa4c61bd7fde472cd811879ae72d6c1d977544
Karina <karinasokolova252@gmail.com>
2025-08-27 18:38:53 +0300

    Add new pins

--- config/importmap.rb
+++ config/importmap.rb
@@ -8,3 +8,5 @@ pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js'
 pin_all_from 'app/javascript/controllers', under: 'controllers'
 pin '@popperjs/core', to: 'popper.js', preload: true
 pin 'bootstrap', to: 'bootstrap.bundle.min.js', preload: true
+pin "trix"
+pin "@rails/actiontext", to: "actiontext.esm.js"
