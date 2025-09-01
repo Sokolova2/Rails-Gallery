1e236306e2ea0d53380884ad5277e9f7828ed941
Karina <karinasokolova252@gmail.com>
2025-09-01 16:36:24 +0300

    Add a new route and setting for active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   get 'set_locale', to: 'application#set_locale'
 
   devise_for :users, controllers: {
