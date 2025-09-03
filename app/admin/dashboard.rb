dc59a16da5ecfe28900355343581b91e012ecc5b
Karina <karinasokolova252@gmail.com>
2025-09-03 22:57:03 +0300

    Add a view last 5 created category and comments. Also last 10 images

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -4,31 +4,49 @@ ActiveAdmin.register_page "Dashboard" do
   menu priority: 1, label: proc { I18n.t("active_admin.dashboard") }
 
   content title: proc { I18n.t("active_admin.dashboard") } do
-    div class: "blank_slate_container", id: "dashboard_default_message" do
-      span class: "blank_slate" do
-        span I18n.t("active_admin.dashboard_welcome.welcome")
-        small I18n.t("active_admin.dashboard_welcome.call_to_action")
+    columns do
+      column do
+        panel "Last 5 created categories" do
+          ul do
+            Category.order(created_at: :desc).limit(5).map do |category|
+              li class: "category-item" do
+                link_to(category.category_name, admin_category_path(category), class: "category-name")
+              end
+            end
+          end
+        end
       end
     end
-
-    # Here is an example of a simple dashboard with columns and panels.
-    #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
-
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
-  end # content
+    columns do
+      column do
+        panel "Last 5 created comments" do
+          ul do
+            Comment.order(created_at: :desc).limit(5).map do |comment|
+              div class: "comment-block" do
+                li class:"comment-item" do
+                  link_to(comment.comment, admin_user_comment_path(comment), class: "comment")
+                end
+                li class:"comment-item" do
+                  link_to(comment.user.email, admin_user_comment_path(comment), class: "comment-email")
+                end
+              end
+            end
+          end
+        end
+      end
+    end
+    columns do
+      column do
+        panel "Last 10 created images" do
+          ul class:"image-grid" do
+            Image.order(created_at: :desc).limit(10).map do |image|
+              li class: "image-item" do
+                link_to(image_tag(image.image.url(:medium)), admin_images_path(image), class: "masonry-item")
+              end
+            end
+          end
+        end
+      end
+    end
+  end
 end
