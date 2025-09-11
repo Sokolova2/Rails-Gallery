c6b7288dcbf0331da65dbd5b087b6f0b294be1db
Karina <karinasokolova252@gmail.com>
2025-09-11 23:14:10 +0300

    Add email which from send mail message

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,17 +3,17 @@
 class UserMailer < ApplicationMailer
   def welcome_email(user)
     @user = user
-    mail(to: @user.email, subject: t('welcome-gallery'))
+    mail(to: @user.email, from: 'karinasokolova252@gmail.com', subject: t('welcome-gallery'))
   end
 
   def subscribe_email(user)
     @user = user
-    mail(to: @user.email, subject: t('subscribe'))
+    mail(to: @user.email, from: 'karinasokolova252@gmail.com', subject: t('subscribe'))
   end
 
   def add_image_email(category, user)
     @category = category
     @users = user
-    mail(to: @users.email, subject: t('new-photo'))
+    mail(to: @users.email, from: 'karinasokolova252@gmail.com', subject: t('new-photo'))
   end
 end
