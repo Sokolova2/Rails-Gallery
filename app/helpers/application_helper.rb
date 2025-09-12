e2b537cefc67ba4972c7e56c8d5946553683eafa
Karina <karinasokolova252@gmail.com>
2025-09-12 23:05:20 +0300

    Change method for user actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def add_user_active(resource, action_type)
+  def add_user_action(resource, action_type)
     UserAction.create(user: resource, action_type: action_type, url: request.original_url)
   end
 end
