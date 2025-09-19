9f1280ce910615779d80fb27c52791a60dfd9b0a
Karina <karinasokolova252@gmail.com>
2025-09-19 20:52:40 +0300

    Change attribute name from comment to body

--- db/migrate/20250818151247_create_comments.rb
+++ db/migrate/20250818151247_create_comments.rb
@@ -6,7 +6,7 @@ class CreateComments < ActiveRecord::Migration[8.0]
       t.belongs_to :user, null: false, foreign_key: true
       t.belongs_to :image, null: false, foreign_key: true
       t.belongs_to :category, null: false, foreign_key: true
-      t.string :comment
+      t.string :body
 
       t.timestamps
     end
