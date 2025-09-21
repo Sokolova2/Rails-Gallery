c995270f0d7f4cf4efc3c25e2ef4b3374f26b5a7
Karina <karinasokolova252@gmail.com>
2025-09-21 16:43:02 +0300

    Fixed action mailer defult url options

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,21 @@ Rails.application.configure do
   config.active_job.queue_adapter = :solid_queue
   config.solid_queue.connects_to = { database: { writing: :queue } }
 
-  config.action_mailer.default_url_options = { host: 'example.com' }
-
   config.i18n.fallbacks = true
 
   config.active_record.dump_schema_after_migration = false
 
   config.active_record.attributes_for_inspect = [:id]
+
+  config.action_mailer.default_url_options = { host: 'rails-gallery-c2134199de34.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'gmail.com',
+    user_name: ENV.fetch('USER_FOR_MAIL', nil),
+    password: ENV.fetch('PASSWORD_FOR_MAIL', nil),
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 end
